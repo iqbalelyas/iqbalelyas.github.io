9ac26f2264bed9094bed90b561973df1774066d1
iqbalelyas <iqbal35mm@gmail.com>
2020-03-24 16:07:36 +0700

    first commit

---
+++ precache-manifest.9a7d1f79a1d7d3c77347e8a8dc88edcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8515f1c93589595f7e9cd09d1a9af035",
+    "url": "/index.html"
+  },
+  {
+    "revision": "154a8da39065db0f5451",
+    "url": "/static/css/main.921eaeec.chunk.css"
+  },
+  {
+    "revision": "242e09d5b3dc81033b08",
+    "url": "/static/js/2.ca5587d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca5587d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "154a8da39065db0f5451",
+    "url": "/static/js/main.75eed131.chunk.js"
+  },
+  {
+    "revision": "a35dcfd05e6848d19fa6",
+    "url": "/static/js/runtime-main.1f9d6cd1.js"
+  },
+  {
+    "revision": "73a55f89a367a988bd965be0d40fe25d",
+    "url": "/static/media/aneka-makanan.73a55f89.jpeg"
+  }
+]);
